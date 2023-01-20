c8ffebe0c87a578ca09ba6476a77d27a008d5f15
eduardocoelholima <eduardocoelholima@gmail.com>
2023-01-20 12:03:43 -0500

    update

---
+++ memo/README.md
@@ -0,0 +1,1 @@
+## files in this directory will log topics covered in each lecture
---
+++ src/intro/HelloWorld/HelloSomethingElse.java
@@ -0,0 +1,8 @@
+package intro.HelloWorld;
+
+public class HelloSomethingElse {
+
+    public static void main(String[] args) {
+        System.out.println("Yeeeeeeey");
+    }
+}
