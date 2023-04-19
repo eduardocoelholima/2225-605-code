eb173d6d773f52e6f09a18f2b3bfc3a7489f1b44
eduardocoelholima <eduardocoelholima@gmail.com>
2023-04-19 14:40:30 -0400

    update

--- src/networking/datagrams/QuoteClient.java
+++ src/networking/datagrams/QuoteClient.java
@@ -29,7 +29,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package datagrams;
+package networking.datagrams;
 
 
 import java.io.*;
--- src/networking/datagrams/QuoteServerThread.java
+++ src/networking/datagrams/QuoteServerThread.java
@@ -30,7 +30,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package datagrams;
+package networking.datagrams;
 
 import java.io.*;
 import java.net.*;
